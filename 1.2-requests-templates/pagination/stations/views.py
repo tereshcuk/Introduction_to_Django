f678d412de1f3224a9acc677001f13ac68b39b6c
Nickolay <tereshcuk@yandex.ru>
2025-05-02 16:40:09 +0300

    first

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -15,12 +15,16 @@ def bus_stations(request):
     page_number = int(request.GET.get('page', 1))
     
     with open(settings.BUS_STATION_CSV, newline='', encoding='utf-8') as csvfile:
-        content = csv.DictReader(csvfile)              
-        paginator = Paginator(content, 10)
-        page = paginator.get_page(page_number)    
-        context = {
+        content = csv.DictReader(csvfile) 
+        bus_stations_list = []
+        for row in content:
+            bus_stations_list.append(row)            
+    
+    paginator = Paginator(bus_stations_list, 10)
+    page = paginator.get_page(page_number)    
+    context = {
                    'bus_stations': page.object_list,
                    'page': page,
                   }    
    
-        return render(request, 'stations/index.html', context)
+    return render(request, 'stations/index.html', context)
