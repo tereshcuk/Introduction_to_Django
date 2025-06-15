7dc422c3783c2f81c64c6165e8c89a7a78dc7e23
tereshcuk <146026696+tereshcuk@users.noreply.github.com>
2025-06-15 19:37:43 +0300

    Delete 3.3-permissions/api_with_restrictions directory

--- 3.3-permissions/api_with_restrictions/advertisements/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-
-class IsOwnerOrReadOnly(BasePermission):
-    
-    def has_object_permission(self, request, view, obj):
-        if request.method == 'GET':
-            return True
-        return request.user == obj.creator
