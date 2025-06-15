ec3726a78ea2c564ea0ec0462cdadbbe77b775f8
tereshcuk <146026696+tereshcuk@users.noreply.github.com>
2025-06-15 19:31:22 +0300

    Add files via upload

---
+++ 3.3-permissions/api_with_restrictions/api_with_restrictions/advertisements/filters.py
@@ -0,0 +1,16 @@
+from django_filters import rest_framework as filters
+
+from advertisements.models import Advertisement
+
+
+class AdvertisementFilter(filters.FilterSet):
+    """Фильтры для объявлений."""
+
+    # TODO: задайте требуемые фильтры
+    created_at = filters.DateFromToRangeFilter(field_name='created_at')
+    status = filters.CharFilter(field_name='status')
+    
+
+    class Meta:
+        model = Advertisement
+        fields = ['created_at', 'status']
